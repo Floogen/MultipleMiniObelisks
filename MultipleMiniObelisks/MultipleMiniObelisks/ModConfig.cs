2dd1846a21296e6440e6a71b68b0eb6bf14fc1aa
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-02-01 07:42:24 -0600

    Created ModConfig

---
+++ MultipleMiniObelisks/MultipleMiniObelisks/ModConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace MultipleMiniObelisks
+{
+    public class ModConfig
+    {
+        public bool AllowMiniObelisksToBePlacedAnywhere { get; set; }
+
+        public ModConfig()
+        {
+            AllowMiniObelisksToBePlacedAnywhere = false;
+        }
+    }
+}
