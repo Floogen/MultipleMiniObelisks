0b0cfeb4e78c5ad2db6eb5fb8f00741102284f64
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-02-10 18:37:54 -0600

    Hooked into ObjectListChanged

--- MultipleMiniObelisks/MultipleMiniObelisks/ModEntry.cs
+++ MultipleMiniObelisks/MultipleMiniObelisks/ModEntry.cs
@@ -43,6 +43,42 @@ namespace MultipleMiniObelisks
                 Monitor.Log($"Issue with Harmony patch: {e}", LogLevel.Error);
                 return;
             }
+            // Hook into ObjectListChanged to catch when Mini-Obelisks are placed / removed
+            helper.Events.World.ObjectListChanged += this.OnObjectListChanged;
+        private void OnObjectListChanged(object sender, ObjectListChangedEventArgs e)
+        {
+            if (!Context.IsMainPlayer)
+            {
+                return;
+            }
+
+            if (!Game1.player.modData.ContainsKey(ObeliskLocationsKey))
+            {
+                Game1.player.modData.Add(ObeliskLocationsKey, JsonConvert.SerializeObject(new List<MiniObelisk>()));
+            }
+
+            List<MiniObelisk> miniObelisks = JsonConvert.DeserializeObject<List<MiniObelisk>>(Game1.player.modData[ObeliskLocationsKey]);
+
+            // Add any new obelisks
+            foreach (var tileToObelisk in e.Added.Where(o => o.Value.ParentSheetIndex == 238 && o.Value.bigCraftable))
+            {
+                StardewValley.Object obelisk = tileToObelisk.Value;
+                if (!obelisk.modData.ContainsKey(ObeliskNameDataKey))
+                {
+                    obelisk.modData.Add(ObeliskNameDataKey, String.Empty);
+                }
+
+                miniObelisks.Add(new MiniObelisk(e.Location.NameOrUniqueName, tileToObelisk.Key, obelisk.modData[ObeliskNameDataKey]));
+            }
+
+            // Remove any removed obelisks
+            foreach (var tileToObelisk in e.Removed.Where(o => o.Value.ParentSheetIndex == 238 && o.Value.bigCraftable))
+            {
+                miniObelisks = miniObelisks.Where(o => !(o.LocationName == e.Location.NameOrUniqueName && o.Tile == tileToObelisk.Key)).ToList();
+            }
+
+            Game1.player.modData[ObeliskLocationsKey] = JsonConvert.SerializeObject(miniObelisks);
+        }
         }
     }
 }
