cb00145b44a79cfbc24d8be0125eaa737cda5945
Mr. Automation <31755155+Floogen@users.noreply.github.com>
2021-02-10 18:34:04 -0600

    Added MiniObelisk class to be used for multiplayer checks

---
+++ MultipleMiniObelisks/MultipleMiniObelisks/Objects/MiniObelisk.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace MultipleMiniObelisks.Objects
+{
+    public class MiniObelisk
+    {
+        public string LocationName { get; set; }
+        public Vector2 Tile { get; set; }
+        public string CustomName { get; set; }
+
+        public MiniObelisk()
+        {
+
+        }
+
+        public MiniObelisk(string locationName, Vector2 tile, string customName)
+        {
+            this.LocationName = locationName;
+            this.Tile = tile;
+            this.CustomName = customName;
+        }
+    }
+}
